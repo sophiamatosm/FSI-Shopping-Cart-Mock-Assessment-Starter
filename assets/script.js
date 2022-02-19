08647b0d1c7ed12f0f3440746aba2fc406dcaed2
Emelinda matos <smiles0721@gmail.com>
2022-02-19 12:45:21 -0500

    added selectors

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+
+let voteforDown = document.querySelector("#quantity-down")
+let voteforUp = document.querySelector("#quantity-up")
+
+console.log("Votes for down", voteforDown)
+console.log("Votes for up",voteforUp )
